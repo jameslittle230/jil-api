d71d42114524990ad3ca05725c602efc4de78e8d
James Little <littleguy23@gmail.com>
2022-07-17 11:31:27 -0400

    Modularize all the guestbook logic

---
+++ src/guestbook/get_single_entry.rs
@@ -0,0 +1,26 @@
+use actix_web::web;
+use anyhow::{Error as AHError, Result};
+use dynomite::AttributeValue;
+
+use crate::AppState;
+
+use super::Entry;
+
+pub(crate) async fn get_single_entry(state: &web::Data<AppState>, id: String) -> Result<Entry> {
+    let entry: Entry = state
+        .dynamodb
+        .query()
+        .table_name("jil-guestbook")
+        .key_condition_expression("id = :value".to_string())
+        .expression_attribute_values(":value".to_string(), AttributeValue::S(id.clone()))
+        .send()
+        .await?
+        // .map_err(ErrorBadRequest)?
+        .items
+        .unwrap()
+        .pop()
+        .ok_or_else(|| AHError::msg(format!("No entry found with ID {id}")))?
+        .try_into()?;
+
+    Ok(entry)
+}
---
+++ src/guestbook/get_undeleted_entries.rs
@@ -0,0 +1,52 @@
+use actix_web::web;
+use anyhow::{Error as AHError, Result};
+use uuid::Uuid;
+
+use crate::AppState;
+
+use super::Entry;
+
+pub(crate) async fn get_undeleted_entries(
+    state: web::Data<AppState>,
+    after: Option<Uuid>,
+) -> Result<(usize, Vec<Entry>)> {
+    let scan_output = state
+        .dynamodb
+        .scan()
+        .table_name("jil-guestbook")
+        .send()
+        .await?;
+
+    let items = scan_output
+        .items
+        .ok_or_else(|| AHError::msg("Could not get items from dynamodb"))?;
+
+    let mut did_see_after_uuid = false;
+
+    let mut entries: Vec<Entry> = items
+        .into_iter()
+        .map(Entry::try_from)
+        .filter_map(|res| res.ok())
+        .filter(|entry| entry.deleted_at.is_none())
+        .collect();
+
+    let total_size = entries.len();
+    entries.sort_by_key(|entry| entry.created_at);
+
+    entries = entries
+        .into_iter()
+        .skip_while(|entry| match after {
+            Some(after) => {
+                if entry.id == after {
+                    did_see_after_uuid = true;
+                    return true; // don't include the one whose ID was specified
+                }
+
+                !did_see_after_uuid
+            }
+            None => false,
+        })
+        .collect();
+
+    Ok((total_size, entries))
+}
